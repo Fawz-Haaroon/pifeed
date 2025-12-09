f01e2efc0e77d739ba9a0873b968436273beae56
Fawz-Haaroon <fawzhaaroon216@gmail.com>
2025-12-09 11:14:30 +0530

    feat(type): add type system for pifeed

---
+++ pifeed/_types/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from pifeed._types._type import ExportList
+
+
+__all__: ExportList = [
+    "ExportList",
+]
+
+
+# NOTE: keep this list sorted
+assert __all__ == sorted(__all__)
---
+++ pifeed/_types/_type.py
@@ -0,0 +1,8 @@
+"""Type definitions for pifeed"""
+
+from __future__ import annotations
+
+from typing import TypeAlias
+
+
+ExportList: TypeAlias = list[str]
